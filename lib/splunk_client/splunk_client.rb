999d8ad4278767dc86d92997625d497d6104e28b
Max Reznichenko <max.reznichenko@gmail.com>
2014-11-17 17:57:12 -0500

    Added earliest / latest search time, other improvements

--- lib/splunk_client/splunk_client.rb
+++ lib/splunk_client/splunk_client.rb
@@ -12,14 +12,10 @@ require File.expand_path File.join(File.dirname(__FILE__), 'splunk_alert_feed')
 
 class SplunkClient
 
-  def initialize(username, password, host, opts = {})
-    @USER=username; @PASS=password; @HOST=host;
-    @PORT = opts[:port] || 8089
-    @READ_TIMEOUT = opts[:read_time_out] || 60
-    @USE_SSL = opts[:use_ssl] || false
-    proxy_url = opts[:proxy_url] || ''
-    
+  def initialize(username, password, host, port=8089, proxy_url = '', read_time_out = 60, use_ssl = true)
+    @USER=username; @PASS=password; @HOST=host; @PORT=port; @READ_TIMEOUT = read_time_out
     @PROXY_URI = URI(proxy_url) if proxy_url && !proxy_url.empty?
+    @use_ssl = use_ssl
 
     sessionKey = get_session_key
 
@@ -30,16 +26,18 @@ class SplunkClient
     end
   end
 
-  def search(search)
-    create_search(search)
+  def search(search, start_time = nil, end_time = nil)
+    create_search(search, start_time, end_time)
   end
 
-  def create_search(search)
-    # Returns a SplunkJob 
-    xml = splunk_post_request("/services/search/jobs",
-                              "search=#{CGI::escape("search #{search}")}",
-                              @SESSION_KEY)
+  # Returns a SplunkJob
+  def create_search(search, start_time = nil, end_time = nil)
+    start_time, end_time = start_time.to_s, end_time.to_s
+    data_string = "search=#{CGI::escape("search #{search}")}"
+    data_string += "&searchEarliestTime=#{CGI.escape(start_time)}" unless end_time.empty?
+    data_string += "&searchLatestTime=#{CGI.escape(end_time)}" unless end_time.empty?
 
+    xml = splunk_post_request("/services/search/jobs", data_string, @SESSION_KEY)
     @doc = Nokogiri::Slop(xml)
 
     return SplunkJob.new(@doc.xpath("//sid").text, self)
@@ -56,12 +54,12 @@ class SplunkClient
     url += "&output_mode=#{mode}" unless mode.nil?
     splunk_get_request(url)
   end
-  
+
   def get_alert_list(user="nobody", count=30)
     xml = splunk_get_request("/servicesNS/#{user}/search/alerts/fired_alerts?count=#{count}")
     SplunkAlertFeed.new(Nokogiri::Slop(xml), self)
   end
-  
+
   def get_alert(alarmName, user="nobody")
     xml = splunk_get_request("/servicesNS/#{user}/search/alerts/fired_alerts/#{alarmName}")
     SplunkAlert.new(Nokogiri::Slop(xml).css("entry")[0], self)
@@ -76,14 +74,14 @@ class SplunkClient
 
   private ###############################################################################
 
-  def splunk_http_request
+  def splunk_http_request()
     if @PROXY_URI
       http = Net::HTTP.new(@HOST, @PORT, @PROXY_URI.host, @PROXY_URI.port)
     else
-      http = Net::HTTP.new(@HOST, @PORT)
+      http = Net::HTTP.new(@HOST, @PORT, nil)
     end
     http.read_timeout = @READ_TIMEOUT
-    http.use_ssl = @SSL
+    http.use_ssl = @use_ssl
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     http
   end
@@ -109,3 +107,6 @@ class SplunkSessionError < SecurityError
   # Exception class for handling invalid session tokens received by the gem
 end
 
+class SplunkWaitTimeout < Exception
+  # Raised when splunk request times out 
+end
